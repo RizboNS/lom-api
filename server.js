b61e3b9b175cb0b19079716cefc78e21035a10d7
rizbons <rizbo.ns.bk@gmail.com>
2022-08-01 11:55:07 +0200

    setting up api

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv/config')
 
+// IMPORT ROUTES
+const postsRoute = require('./routes/posts')
+
+// MIDDLEWARES
+app.use('/posts', postsRoute)
+
+
+
+// ROUTES
 app.get('/', (req, res) => {
-    console.log('Here')
-    res.status(200).json({ message: "Error"})
+    res.send('hello from darkness')
 })
 
+
+// CONNECT TO DB
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log('Connected to the DB!')
+})
+
+
+
+// LISTENER
 app.listen(3000)
