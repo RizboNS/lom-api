73a1b68bf6dd98289a3633fa8f514eaa1eed18d7
rizbons <rizbo.ns.bk@gmail.com>
2022-08-02 11:01:17 +0200

    createCustomer

--- routes/customers.js
+++ routes/customers.js
@@ -2,9 +2,26 @@ const express = require('express')
 const router = express.Router()
 const Customer = require('../models/Customer')
 
-router.get('/', (req, res) => {
-    res.send('hi from customers')
+// Create customer
+router.post('/', async (req, res) => {
+    const customer = new Customer({
+        name: req.body.name,
+        lastName: req.body.lastName,
+        streetName: req.body.streetName,
+        streetNum: req.body.streetNum,
+        state: req.body.state,
+        zip: req.body.zip,
+        // TO DO ORDER LIST
+    })
+    try {
+        const savedCustomer = await customer.save()
+        res.json(savedCustomer)
+    } catch (err) {
+        res.json({message: err})
+    }
 })
+
+
 
 
 module.exports = router
