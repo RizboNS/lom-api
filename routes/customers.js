835b638ef60f67360f9a30a3e185b66e86e2be8a
rizbons <rizbo.ns.bk@gmail.com>
2022-08-03 15:46:15 +0200

    PATCH customer

--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,28 @@ const express = require('express')
 const router = express.Router()
 const Customer = require('../models/Customer')
 
+// Get Customer by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const customer = await Customer.findById(req.params.id)
+        res.json(customer)
+    } catch (err) {
+        res.json({message: err})       
+    }
+})
+
+// Get all customers
+router.get('/', async (req, res) => {
+    try {
+        const customers = await Customer.find()
+        res.json(customers)
+    } catch (err) {
+        res.json({message: err})
+    }
+})
+
+
+
 // Create customer
 router.post('/', async (req, res) => {
     const customer = new Customer({
@@ -21,7 +43,26 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Update customer
+router.patch('/:id',async (req, res) => {
+    try {
+        const updatedCustomer = await Customer.updateOne(
+            {_id: req.params.id},
+            {$set: {
+                name: req.body.name,
+                lastName: req.body.lastName,
+                streetName: req.body.streetName,
+                streetNum: req.body.streetNum,
+                state: req.body.state,
+                zip: req.body.zip, 
+            }}
+        )
+        res.json(updatedCustomer)
+    } catch (err) {
+        res.json({message: err})      
+    }
 
+})
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv/config')
+
+//  CORS
+app.use(cors({
+    origin: '*'
+}))
 
 
 // BODY-PARSER
